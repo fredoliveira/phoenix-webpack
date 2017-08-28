6c38dfd0403f07178bccc1e7a63828e042683271
Fred Oliveira <fred@helloform.com>
2017-08-28 16:32:55 +0100

    CSS loader, extraction webpack config

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,25 +1,46 @@
 const path = require("path");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: ['./js/app.js'],
+  entry: [
+    './css/app.css',
+    './js/app.js'
+  ],
+
   output: {
     path: path.resolve(__dirname, "../priv/static"),
     filename: 'js/app.js',
   },
+
   plugins: [
     new CopyWebpackPlugin([{
       from: './static',
       to: path.resolve(__dirname, "../priv/static")
     }]),
+
+    new ExtractTextPlugin({
+      filename: "css/app.css",
+      allChunks: true
+    }),
   ],
+
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
+        use: {
+          loader: 'babel-loader'
+        }
+      }, {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
     ],
   },
 }
